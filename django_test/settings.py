0062f6e0d6d0c626b9e16aaa072a4dd2a423ffd5
Tom Forbes <tom@tomforb.es>
2020-05-17 16:15:49 +0100

    Add tests

--- django_test/settings.py
+++ django_test/settings.py
@@ -56,8 +56,13 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
         'OPTIONS': {
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ])
+            ],
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -65,8 +70,9 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-    },
+    }
 ]
+
 
 WSGI_APPLICATION = 'django_test.wsgi.application'
 
